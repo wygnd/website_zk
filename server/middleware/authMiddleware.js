4bd256fce17b6e67740fec6037311bded8ad432d
Denis <netfixk@yandex.ru>
2023-09-11 14:36:14 +0300

    Require login method

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    if (req.method = "OPTIONS") next();
-
+    if (req.method = "OPTIONS") {
+        next();
+    }
     try {
         const token = req.headers.authorization.split(' ')[1];
         if (!token) {
