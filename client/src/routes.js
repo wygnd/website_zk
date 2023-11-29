33173e0216822b4ad35e697a620fe56a09b9157c
Denis <netfixk@yandex.ru>
2023-11-29 18:04:59 +0300

    Init for exam

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,36 +1,63 @@
-import Admin from "./pages/Admin/Admin"
-import Auth from "./pages/Auth/Auth"
-import Main from "./pages/Main/Main"
-import Pirvacy from "./pages/Privacy/Privacy"
-import NotFound from "./pages/NotFound/NotFound"
+import Admin from "./pages/Admin/Admin";
+import Auth from "./pages/Auth/Auth";
+import Main from "./pages/Main/Main";
+import Pirvacy from "./pages/Privacy/Privacy";
+import NotFound from "./pages/NotFound/NotFound";
 import {
-    ADMIN_ROUTE,
-    LOGIN_ROUTE,
-    MAIN_ROUTE,
-    MAIN_BLOCK_ITEM_ROUTE,
-    NOTFOUND_ROUTE,
-    TOUR_BLOCK_ITEM_ROUTE,
-    PRIVACY_ROUTE,
-    LOGIN_ROUTE_BREADCRUMB,
-    PRIVACY_ROUTE_BREADCRUMB,
-    NOTFOUND_ROUTE_BREADCRUMB,
-    ADMIN_ROUTE_BREADCRUMB
-} from "./utils/consts"
-import MainBlockItem from './pages/ItemEdit/MainBlockItem/MainBlockItem';
-import TourItem from "./pages/ItemEdit/TourItem/TourItem"
+  ADMIN_ROUTE,
+  LOGIN_ROUTE,
+  MAIN_ROUTE,
+  MAIN_BLOCK_ITEM_ROUTE,
+  NOTFOUND_ROUTE,
+  TOUR_BLOCK_ITEM_ROUTE,
+  PRIVACY_ROUTE,
+  LOGIN_ROUTE_BREADCRUMB,
+  PRIVACY_ROUTE_BREADCRUMB,
+  NOTFOUND_ROUTE_BREADCRUMB,
+  PANORAMA_ROUTE,
+  PANORAMA_ROUTE_BREADCRUMB,
+} from "./utils/consts";
+import MainBlockItem from "./pages/ItemEdit/MainBlockItem/MainBlockItem";
+import TourItem from "./pages/ItemEdit/TourItem/TourItem";
+import Panorama from "./components/Panorama/Panorama";
 
 export const authRoutes = [
-    { path: ADMIN_ROUTE, component: <Admin /> },
-    { path: MAIN_ROUTE, component: <Main />, breadcrumb: null },
-    { path: LOGIN_ROUTE, component: <Auth />, breadcrumb: LOGIN_ROUTE_BREADCRUMB },
-    { path: NOTFOUND_ROUTE, component: <NotFound />, breadcrumb: NOTFOUND_ROUTE_BREADCRUMB },
-    { path: MAIN_BLOCK_ITEM_ROUTE + '/:id', component: <MainBlockItem /> },
-    { path: TOUR_BLOCK_ITEM_ROUTE + '/:id', component: <TourItem /> },
-]
+  { path: ADMIN_ROUTE, component: <Admin /> },
+  { path: MAIN_ROUTE, component: <Main />, breadcrumb: null },
+  {
+    path: LOGIN_ROUTE,
+    component: <Auth />,
+    breadcrumb: LOGIN_ROUTE_BREADCRUMB,
+  },
+  {
+    path: NOTFOUND_ROUTE,
+    component: <NotFound />,
+    breadcrumb: NOTFOUND_ROUTE_BREADCRUMB,
+  },
+  { path: MAIN_BLOCK_ITEM_ROUTE + "/:id", component: <MainBlockItem /> },
+  { path: TOUR_BLOCK_ITEM_ROUTE + "/:id", component: <TourItem /> },
+];
 
 export const publicRoutes = [
-    { path: MAIN_ROUTE, component: <Main />, breadcrumb: null },
-    { path: LOGIN_ROUTE, component: <Auth />, breadcrumb: LOGIN_ROUTE_BREADCRUMB },
-    { path: PRIVACY_ROUTE, component: <Pirvacy />, breadcrumb: PRIVACY_ROUTE_BREADCRUMB },
-    { path: NOTFOUND_ROUTE, component: <NotFound />, breadcrumb: NOTFOUND_ROUTE_BREADCRUMB },
-]
+  { path: MAIN_ROUTE, component: <Main />, breadcrumb: null },
+  {
+    path: LOGIN_ROUTE,
+    component: <Auth />,
+    breadcrumb: LOGIN_ROUTE_BREADCRUMB,
+  },
+  {
+    path: PRIVACY_ROUTE,
+    component: <Pirvacy />,
+    breadcrumb: PRIVACY_ROUTE_BREADCRUMB,
+  },
+  {
+    path: NOTFOUND_ROUTE,
+    component: <NotFound />,
+    breadcrumb: NOTFOUND_ROUTE_BREADCRUMB,
+  },
+  {
+    path: PANORAMA_ROUTE,
+    component: <Panorama />,
+    breadcrumb: PANORAMA_ROUTE_BREADCRUMB,
+  },
+];
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,78 +1,87 @@
-const ApiError = require('../error/ApiError');
-const { Users } = require('../models/models');
-const bcrypt = require('bcrypt');
-const { validationResult } = require('express-validator');
-const userService = require('../service/userService');
+const ApiError = require("../error/ApiError");
+const { Users } = require("../models/models");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const userService = require("../service/userService");
 
 class UsersController {
-    async registration(req, res, next) {
-        try {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return next(ApiError.BadRequest('Ошибка валидации', errors.array()));
-            }
-            const { email, password } = req.body;
-            const userData = await userService.registration(email, password);
-            res.cookie('tokenRef', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
-            return res.json(userData);
-        } catch (error) {
-            next(error)
-        }
+  async registration(req, res, next) {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest("Ошибка валидации", errors.array()));
+      }
+      const { email, password } = req.body;
+      const userData = await userService.registration(email, password);
+      res.cookie("tokenRef", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async login(req, res, next) {
-        try {
-            const { email, password } = req.body
-            const userData = await userService.login(email, password);
-            res.cookie('tokenRef', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
-            return res.json(userData);
-        } catch (error) {
-            next(error);
-        }
+  async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      const userData = await userService.login(email, password);
+      res.cookie("tokenRef", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async logout(req, res, next) {
-        try {
-            const { tokenRef } = req.cookies;
-            const token = await userService.logout(tokenRef);
-            res.clearCookie('tokenRef');
-            if (token) {
-                return res.json({ message: 'Выход выполнен' })
-            }
-            return res.json(token);
-        } catch (error) {
-            next(error);
-        }
+  async logout(req, res, next) {
+    try {
+      const { tokenRef } = req.cookies;
+      const token = await userService.logout(tokenRef);
+      res.clearCookie("tokenRef");
+      if (token) {
+        return res.json({ message: "Выход выполнен" });
+      }
+      return res.json(token);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async check(req, res, next) {
-        try {
-            const token = generateJwt(req.user.id, req.user.email, req.user.role)
-            return res.json({ token })
-        } catch (error) {
-            next(ApiError.badRequest(error.message))
-        }
+  async check(req, res, next) {
+    try {
+      const token = generateJwt(req.user.id, req.user.email, req.user.role);
+      return res.json({ token });
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
     }
+  }
 
-    async refresh(req, res, next) {
-        try {
-            const { tokenRef } = req.cookies;
-            const userData = await userService.refresh(tokenRef);
-            res.cookie('tokenRef', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
-            return res.json(userData);
-        } catch (error) {
-            next(ApiError.BadRequest(error.message));
-        }
+  async refresh(req, res, next) {
+    try {
+      const { tokenRef } = req.cookies;
+      const userData = await userService.refresh(tokenRef);
+      res.cookie("tokenRef", userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
+      return res.json(userData);
+    } catch (error) {
+      next(ApiError.BadRequest(error.message));
     }
-    
-    async getAll(req, res, next) {
-        try {
-            const userData = await userService.getAll();
-            return res.json(userData);
-        } catch (error) {
-            next(ApiError.BadRequest(error.message));
-        }
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const userData = await userService.getAll();
+      return res.json(userData);
+    } catch (error) {
+      next(ApiError.BadRequest(error.message));
     }
+  }
 }
 
 module.exports = new UsersController();
